0b74c56febac1688a46216976d9e5bece941d5b7
Aluno.Tarde <Aluno.Tarde@PositivoMaster2>
2024-04-02 14:17:42 -0300

    troquei o nome , seus puto

---
+++ IdealFashion/src/ordem.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+public class ordem {
+    
+    private String Email;
+        private String Senha;
+       
+     public ordem(String Email, String Senha){
+            this.Email = Email;
+            this.Senha = Senha;
+        }
+
+    /**
+     * @return the Email
+     */
+    public String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param Email the Email to set
+     */
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    /**
+     * @return the Senha
+     */
+    public String getSenha() {
+        return Senha;
+    }
+
+    /**
+     * @param Senha the Senha to set
+     */
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+}
+
